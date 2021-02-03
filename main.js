20a2632dc66fdc58b6c87603f25f4b04942675ee
moshe abecasis <mosh14abou@yahoo.com>
2021-02-03 21:59:06 +0200

    4

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const $body=document.getElementById('body');
-const $container=document.getElementById('container');
-const $button=document.getElementById('button');
+const $body = document.getElementById('body');
+const $container = document.getElementById('container');
+const $button = document.getElementById('button');
 
 
 
 const bgChange = (event)=>{
-  const bgColors = ['lightgreen','lightblue','crimson','lightpink']; 
+  const bgColors = ['green','blue','crimson','pink']; 
   const randomNumber = math.random() * bgColors.length;
   const randomIndex = math.floor(randomNumber);
   $body.style.backgroundColor = bgColors[randomIndex];
