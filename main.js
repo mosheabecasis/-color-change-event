eab67a1b322ef7db804fa42a482a9b4153642997
moshe abecasis <mosh14abou@yahoo.com>
2021-02-04 12:16:17 +0200

    final

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const $body = document.getElementById('body');
 const $container = document.getElementById('container');
 const $button = document.getElementById('button');
 
-
-
-const bgChange = ()=>{
+const bgChange = ()=> {
   const bgColors = ['green','blue','crimson','pink']; 
-  const randomNumber = math.random() * bgColors.length;
-  const randomIndex = math.floor(randomNumber);
+  const randomNumber = Math.random() * bgColors.length;
+  const randomIndex = Math.floor(randomNumber);
   $body.style.backgroundColor = bgColors[randomIndex];
 }
 $button.addEventListener('click',bgChange);
+
+const bgChange2 = ()=> {
+  const containerColors = ['yellow', 'red', 'orange', 'gold'];
+  const randomNumber = Math.random() * containerColors.length;
+  const randomIndex = Math.floor(randomNumber);
+  $container.style.backgroundColor = containerColors[randomIndex];
+}
+$container.addEventListener('click',bgChange2);
